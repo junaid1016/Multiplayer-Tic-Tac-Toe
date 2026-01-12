1eaaeaaf1b855ab0126e36277a803237ada31cd8
junaid1016 <ja2318558@gmail.com>
2026-01-12 23:15:16 +0530

    end point

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:3001/", {
+const socket = io("https://multiplayer-tic-tac-toe-xlhz.vercel.app:3001/", {
   autoConnect: false,
 });
 
